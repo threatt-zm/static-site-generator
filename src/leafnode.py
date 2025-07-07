bfd907094e3ceb028b9740560cd5330cd47ce354
threatt-zm <10990825@live.mercer.edu>
2025-07-07 18:58:50 -0400

    program to render HTML strings with test cases

---
+++ src/leafnode.py
@@ -0,0 +1,12 @@
+from htmlnode import HTMLNode
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError()
+        if self.tag is None:
+            return fr"{self.value}"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
---
+++ src/test_leafnode.py
@@ -0,0 +1,18 @@
+import unittest
+from leafnode import LeafNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_value_error(self):
+        node = LeafNode("h1", None)
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_attributes(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+if __name__ == "__main__":
+    unittest.main()
