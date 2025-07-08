fcacbe0b1177d8ca07b6132f311b1f4e7a6bc31a
threatt-zm <10990825@live.mercer.edu>
2025-07-08 02:17:17 -0400

    program to convert text nodes to html nodes with test cases

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from leafnode import LeafNode
 
 class TextType(Enum):
     PLAIN_TEXT = "plain"
@@ -19,3 +20,20 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.PLAIN_TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD_TEXT:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC_TEXT:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE_TEXT:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception("Invalid text type")
