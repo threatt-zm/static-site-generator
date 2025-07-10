641d00a6c7b2d43768219137c73804947be7bcf2
threatt-zm <10990825@live.mercer.edu>
2025-07-10 06:11:45 -0400

    added function to determine block type from markdown with test cases

--- src/blockfunctions.py
+++ src/blockfunctions.py
@@ -1,7 +1,23 @@
-
+import re
+from block import BlockType
 
 def markdown_to_blocks(markdown):
     split_blocks = markdown.split("\n\n")
     strip_blocks = list(map(lambda x: x.strip(), split_blocks))
-    new_blocks = filter(lambda x: x != "", strip_blocks)
+    new_blocks = list(filter(lambda x: x != "", strip_blocks))
     return new_blocks
+
+def block_to_block_type(markdown):
+    lines = markdown.split("\n")
+
+    if all(list(map(lambda x: re.match(r"^#{1,6} \w+", x), lines))):
+        return BlockType.HEADING
+    if all(list(map(lambda x: x.startswith("```") and x.endswith("```"), lines))):
+        return BlockType.CODE
+    if all(list(map(lambda x: x.startswith(">"), lines))):
+        return BlockType.QUOTE
+    if all(list(map(lambda x: x.startswith("- "), lines))):
+        return BlockType.UNORDERED_LIST
+    if all(list(map(lambda x: x.startswith(f"{lines.index(x)+1}. "), lines))):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
---
+++ src/test_blockfunctions.py
@@ -0,0 +1,57 @@
+import unittest
+from blockfunctions import markdown_to_blocks, block_to_block_type
+from block import BlockType
+
+class TestNodeFunctions(unittest.TestCase):    
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+    def test_heading(self):
+        md = "#### This is a heading"
+        blocktype = block_to_block_type(md)
+        self.assertEqual(BlockType.HEADING, blocktype)
+
+    def test_code_block(self):
+        md = "``` This is a code block ```"
+        blocktype = block_to_block_type(md)
+        self.assertEqual(BlockType.CODE, blocktype)
+
+    def test_quote_block(self):
+        md = "> This is a quote that\n> is broken into \n> multiple lines"
+        blocktype = block_to_block_type(md)
+        self.assertEqual(BlockType.QUOTE, blocktype)
+
+    def test_unordered_list(self):
+        md = "- This is the first item of the unordered list\n- This is the second item of the unordered list\n- This is the third item of the unordered list"
+        blocktype = block_to_block_type(md)
+        self.assertEqual(BlockType.UNORDERED_LIST, blocktype)
+
+    def test_ordered_list(self):
+        md = "1. This is the first item of the ordered list\n2. This is the second item of the ordered list\n3. This is the third item of the ordered list"
+        blocktype = block_to_block_type(md)
+        self.assertEqual(BlockType.ORDERED_LIST, blocktype)
+
+    def test_paragraph(self):
+        md = "This is a normal paragraph"
+        blocktype = block_to_block_type(md)
+        self.assertEqual(BlockType.PARAGRAPH, blocktype)
+
+if __name__ == "__main__":
+    unittest.main() 
