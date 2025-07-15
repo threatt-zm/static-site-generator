ae36a006c6310a6e42437087466569901f811f70
threatt-zm <10990825@live.mercer.edu>
2025-07-15 19:05:03 -0400

    added functions for markdown to htmlnode conversion with test cases

--- src/blockfunctions.py
+++ src/blockfunctions.py
@@ -1,5 +1,8 @@
 import re
 from block import BlockType
+from textnode import TextType, TextNode
+from nodefunctions import text_to_textnodes, text_node_to_html_node
+from parentnode import ParentNode
 
 def markdown_to_blocks(markdown):
     split_blocks = markdown.split("\n\n")
@@ -21,3 +24,61 @@ def block_to_block_type(block):
     if all(list(map(lambda x: x.startswith(f"{lines.index(x)+1}. "), lines))):
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
+
+def text_to_children(text):
+    html_nodes = []
+    nodes = text_to_textnodes(text)
+    for node in nodes:
+        if isinstance(node, TextNode):
+            html_nodes.append(text_node_to_html_node(node))
+        else:
+            html_nodes.append(node)
+    return html_nodes
+def block_to_html_node(block, blocktype):
+    match blocktype:
+        case BlockType.CODE:
+            code_text = block.strip("`").lstrip("\n")
+            code_node = text_node_to_html_node(TextNode(code_text, TextType.CODE_TEXT))
+            return ParentNode("pre", [code_node])
+        case BlockType.HEADING:
+            heading_number = block.count("#")
+            heading_text = block.lstrip("# ")
+            heading_node = text_to_children(heading_text)
+            return ParentNode(f"h{heading_number}", heading_node)
+        case BlockType.QUOTE:
+            lines = block.lstrip("> ").split("\n")
+            quote_children = []
+            for text in lines:
+                quote_nodes = text_to_children(text.lstrip(">"))
+                quote_children.extend(quote_nodes)
+            return ParentNode("blockquote", quote_children)
+        case BlockType.UNORDERED_LIST:
+            lines = block.split("\n")
+            ul_children = []
+            for text in lines:
+                ul_nodes = text_to_children(text.lstrip("- "))
+                ul_children.append(ParentNode("li", ul_nodes))
+            return ParentNode("ul", ul_children)
+        case BlockType.ORDERED_LIST:
+            lines = block.split("\n")
+            ol_children = []
+            number = 1
+            for text in lines:
+                ol_nodes = text_to_children(text.lstrip(f"{number}. "))
+                ol_children.append(ParentNode("li", ol_nodes))
+                number += 1
+            return ParentNode("ol", ol_children)
+        case BlockType.PARAGRAPH:
+            text = " ".join(block.split("\n"))
+            p_nodes = text_to_children(text)
+            return ParentNode("p", p_nodes)
+    
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+
+    html_children = []
+    for block in blocks:
+        blocktype = block_to_block_type(block)
+        html_children.append(block_to_html_node(block, blocktype))
+    
+    return ParentNode("div", html_children)
--- src/test_blockfunctions.py
+++ src/test_blockfunctions.py
@@ -1,5 +1,5 @@
 import unittest
-from blockfunctions import markdown_to_blocks, block_to_block_type
+from blockfunctions import markdown_to_blocks, block_to_block_type, markdown_to_html_node
 from block import BlockType
 
 class TestNodeFunctions(unittest.TestCase):    
@@ -23,7 +23,7 @@ This is the same paragraph on a new line
             ],
         )
 
-    def test_heading(self):
+    def test_block_to_block_type(self):
         md = "#### This is a heading"
         blocktype = block_to_block_type(md)
         self.assertEqual(BlockType.HEADING, blocktype)
@@ -47,6 +47,94 @@ This is the same paragraph on a new line
         md = "This is a normal paragraph"
         blocktype = block_to_block_type(md)
         self.assertEqual(BlockType.PARAGRAPH, blocktype)
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
+
+    def test_quote(self):
+        md = """
+> This is a quote block
+> that contains a **bolded phrase** and
+> an italic _word_
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote>This is a quote block that contains a <b>bolded phrase</b> and an italic <i>word</i></blockquote></div>",
+        )
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+    
+    def test_heading(self):
+        md = """
+### This is a h3 heading with `code`
+
+##### This is a h5 heading with _italic_
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h3>This is a h3 heading with <code>code</code></h3><h5>This is a h5 heading with <i>italic</i></h5></div>"
+        )
+
+    def test_unordered_list(self):
+        md = """
+- Item with no inline markdown
+- Item with **bolded**
+- Item with _italic_        
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>Item with no inline markdown</li><li>Item with <b>bolded</b></li><li>Item with <i>italic</i></li></ul></div>"
+        )
+
+    def test_unordered_list(self):
+        md = """
+1. Item with no inline markdown
+2. Item with **bolded** and ![image](https://i.imgur.com/zjjcJKZ.png)
+3. Item with _italic_ and a link [to boot dev](https://www.boot.dev)       
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            '<div><ol><li>Item with no inline markdown</li><li>Item with <b>bolded</b> and <img src="https://i.imgur.com/zjjcJKZ.png" alt="image"></img></li><li>Item with <i>italic</i> and a link <a href="https://www.boot.dev">to boot dev</a></li></ol></div>'
+        )
 
 if __name__ == "__main__":
     unittest.main() 
