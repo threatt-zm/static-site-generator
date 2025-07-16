96b35c8a9a1cb9b69a9b7c596f8324a85c5c7951
threatt-zm <10990825@live.mercer.edu>
2025-07-16 03:09:30 -0400

    recursively copy files from static directory to public directory with test files

--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+def copy_static(src, dest):
+    base_dir = "/home/threatt_zm/static-site-generator"
+    src_path = os.path.join(base_dir, src)
+    dest_path = os.path.join(base_dir, dest)
+
+    try:
+        if os.path.exists(dest_path):
+            shutil.rmtree(dest_path)
+        os.mkdir(dest_path)
+        if os.path.exists(src_path):
+            for item in os.listdir(src_path):
+                file_path = os.path.join(src_path, item)
+                if os.path.isfile(file_path):
+                    shutil.copy(file_path, dest_path)
+                else:
+                    new_src = os.path.join(src, item)
+                    new_dest = os.path.join(dest, item)
+                    copy_static(new_src, new_dest)
+    except Exception as e:
+        print(f"Exception encountered: {e}")
 
 def main():
-    my_textnode = TextNode("This text is bolded", TextType.BOLD_TEXT)
-
-    print(my_textnode)
+    copy_static("static", "public")
 
 
 if __name__ == '__main__':
