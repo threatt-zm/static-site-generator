12d6b4aa0546a7300cc10a2a565ab357d14cff07
threatt-zm <10990825@live.mercer.edu>
2025-07-17 00:49:19 -0400

    updated for GitHub Pages deployment

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
-import os
-import shutil
+import sys
 from pagemethods import copy_static, generate_pages_recursive
 
 def main():
-    copy_static("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    
+    print(basepath)
+    copy_static("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 
 if __name__ == '__main__':
--- src/pagemethods.py
+++ src/pagemethods.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from blockfunctions import markdown_to_html_node
 
 def copy_static(src, dest):
@@ -28,7 +29,7 @@ def extract_title(markdown):
             return line.strip("# ")
     raise ValueError("h1 header not found")
 
-def generate_page(src_path, template_path, dest_path):
+def generate_page(src_path, template_path, dest_path, basepath):
     print(f"Generating page from {src_path} to {dest_path} using {template_path}")
 
     base_path = "/home/threatt_zm/static-site-generator"
@@ -46,7 +47,7 @@ def generate_page(src_path, template_path, dest_path):
         node = markdown_to_html_node(md)
         html_string = node.to_html()
         title = extract_title(md)
-        new_template = template.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
+        new_template = template.replace("{{ Title }}", title).replace("{{ Content }}", html_string).replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
 
         if not os.path.exists(os.path.dirname(dest)):
             os.makedirs(os.path.dirname(dest))
@@ -59,7 +60,7 @@ def generate_page(src_path, template_path, dest_path):
     except Exception as e:
         print(f"Exception found: {e}")
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     base_path = "/home/threatt_zm/static-site-generator"
     src = os.path.join(base_path, dir_path_content)
     temp = os.path.join(base_path, template_path)
@@ -71,8 +72,8 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             new_dest = os.path.join(dest_dir_path, item)
             item_path = os.path.join(src, item)
             if os.path.isfile(item_path):
-                generate_page(new_src, template_path, new_dest.replace(".md", ".html"))
+                generate_page(new_src, template_path, new_dest.replace(".md", ".html"), basepath)
             else:
-                generate_pages_recursive(new_src, template_path, new_dest)
+                generate_pages_recursive(new_src, template_path, new_dest, basepath)
     except:
         print(f"Exception found: {e}")  
