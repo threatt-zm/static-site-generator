0668786bda213d9e52fe012ade87e9d46082486f
threatt-zm <10990825@live.mercer.edu>
2025-07-16 23:04:33 -0400

    now generates additional pages, recursively

--- src/main.py
+++ src/main.py
@@ -1,32 +1,10 @@
 import os
 import shutil
-from pagemethods import generate_page
-
-
-def copy_static(src, dest):
-    base_dir = "/home/threatt_zm/static-site-generator"
-    src_path = os.path.join(base_dir, src)
-    dest_path = os.path.join(base_dir, dest)
-
-    try:
-        if os.path.exists(dest_path):
-            shutil.rmtree(dest_path)
-        os.mkdir(dest_path)
-        if os.path.exists(src_path):
-            for item in os.listdir(src_path):
-                file_path = os.path.join(src_path, item)
-                if os.path.isfile(file_path):
-                    shutil.copy(file_path, dest_path)
-                else:
-                    new_src = os.path.join(src, item)
-                    new_dest = os.path.join(dest, item)
-                    copy_static(new_src, new_dest)
-    except Exception as e:
-        print(f"Exception encountered: {e}")
+from pagemethods import copy_static, generate_pages_recursive
 
 def main():
     copy_static("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 
 if __name__ == '__main__':
--- src/pagemethods.py
+++ src/pagemethods.py
@@ -1,6 +1,27 @@
 import os
 from blockfunctions import markdown_to_html_node
 
+def copy_static(src, dest):
+    base_dir = "/home/threatt_zm/static-site-generator"
+    src_path = os.path.join(base_dir, src)
+    dest_path = os.path.join(base_dir, dest)
+
+    try:
+        if os.path.exists(dest_path):
+            shutil.rmtree(dest_path)
+        os.mkdir(dest_path)
+        if os.path.exists(src_path):
+            for item in os.listdir(src_path):
+                file_path = os.path.join(src_path, item)
+                if os.path.isfile(file_path):
+                    shutil.copy(file_path, dest_path)
+                else:
+                    new_src = os.path.join(src, item)
+                    new_dest = os.path.join(dest, item)
+                    copy_static(new_src, new_dest)
+    except Exception as e:
+        print(f"Exception encountered: {e}")
+
 def extract_title(markdown):
     for line in markdown.split("\n"):
         if line.startswith("# "):
@@ -37,3 +58,21 @@ def generate_page(src_path, template_path, dest_path):
         open_dest.close()
     except Exception as e:
         print(f"Exception found: {e}")
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    base_path = "/home/threatt_zm/static-site-generator"
+    src = os.path.join(base_path, dir_path_content)
+    temp = os.path.join(base_path, template_path)
+    dest = os.path.join(base_path, dest_dir_path)
+
+    try:
+        for item in os.listdir(src):
+            new_src = os.path.join(dir_path_content, item)
+            new_dest = os.path.join(dest_dir_path, item)
+            item_path = os.path.join(src, item)
+            if os.path.isfile(item_path):
+                generate_page(new_src, template_path, new_dest.replace(".md", ".html"))
+            else:
+                generate_pages_recursive(new_src, template_path, new_dest)
+    except:
+        print(f"Exception found: {e}")  
